ff5ab84a7ebc46c3afb53acf978f095fcfb1ce07
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-10-24 19:42:21 +0900

    Refactor minefield package

--- java101/src/main/java/com/ayse/minefield/MineSweeper.java
+++ java101/src/main/java/com/ayse/minefield/MineSweeper.java
@@ -5,110 +5,116 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Mine Sweeper Game
+ * The game is played with a matrix of row x column.
+ * The game matrix is filled with mines and the player tries to find the mines by entering the row and column indexes.
+ * If the player finds all the mines, the game is completed successfully.
+ * Otherwise, the game is over when the player finds a mine.
+ *
+ * @author aysedemirel
+ */
 public class MineSweeper {
+    private final int row;
+    private final int column;
+    private final Scanner scanner;
+    private int success = 0;
+    private char[][] gameMatrix;
 
-  private final int row;
-  private final int column;
-  private final Scanner scanner;
-  private int success = 0;
-  private char[][] gameMatrix;
-  private Map<Integer, Integer> mineIndex;
-
-  public MineSweeper() {
-    scanner = new Scanner(System.in);
-    System.out.println("Welcome to the minefield game....");
-    System.out.print("Row: ");
-    row = scanner.nextInt();
-    System.out.print("Column: ");
-    column = scanner.nextInt();
-    if (row * column > 0) {
-      success = row * column;
-      fullMatrix();
-      startGame();
-    } else {
-      System.out.println("Exit....");
-    }
-  }
-
-  public static void main(String[] args) {
-    new MineSweeper();
-  }
-
-  private void startGame() {
-
-    int rowIndex = -1;
-    int columnIndex = -1;
-    boolean isFinish = false;
-    while (!isFinish) {
-      boolean isBoundFalse = true;
-      while (isBoundFalse) {
-        System.out.print("Enter row index:");
-        rowIndex = scanner.nextInt();
-        System.out.print("Enter column index:");
-        columnIndex = scanner.nextInt();
-        isBoundFalse =
-            (rowIndex >= row || rowIndex < 0) || (columnIndex >= column || columnIndex < 0);
-        if (isBoundFalse) {
-          System.out.println("Again....");
+    public MineSweeper() {
+        scanner = new Scanner(System.in);
+        System.out.println("Welcome to the minefield game....");
+        System.out.print("Row: ");
+        row = scanner.nextInt();
+        System.out.print("Column: ");
+        column = scanner.nextInt();
+        if (row * column > 0) {
+            success = row * column;
+            fullMatrix();
+            startGame();
+        } else {
+            System.out.println("Exit....");
         }
-      }
-      if (gameMatrix[rowIndex][columnIndex] == '*') {
-        isFinish = true;
-        System.out.println("BOOOMMM!!!");
-        break;
-      }
-      int gain = 0;
-      if (columnIndex < (gameMatrix[0].length - 1)) {
-        gain += gameMatrix[rowIndex][columnIndex + 1] == '*' ? 1 : 0;
-      }
-      if (columnIndex > 0) {
-        gain += gameMatrix[rowIndex][columnIndex - 1] == '*' ? 1 : 0;
-      }
-      if (rowIndex > 0) {
-        gain += gameMatrix[rowIndex - 1][columnIndex] == '*' ? 1 : 0;
-      }
-      if (rowIndex < (gameMatrix.length - 1)) {
-        gain += gameMatrix[rowIndex + 1][columnIndex] == '*' ? 1 : 0;
-      }
-      gameMatrix[rowIndex][columnIndex] = Character.forDigit(gain, 10);
-      success--;
-      if (success == 0) {
-        isFinish = true;
-        System.out.println("CONGRATULATIONS!!!!");
-      }
-      printMatrix();
-    }
-  }
-
-  private void fullMatrix() {
-    gameMatrix = new char[row][column];
-    int len = row * column;
-    int randomCount = len / 4;
-    mineIndex = new HashMap<>();
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < column; j++) {
-        gameMatrix[i][j] = '-';
-      }
     }
 
-    while (randomCount != 0) {
-      Random random = new Random();
-      int randomRow = random.nextInt(0, row);
-      int randomColumn = random.nextInt(0, column);
-      mineIndex.put(randomRow, randomColumn);
-      gameMatrix[randomRow][randomColumn] = '*';
-      randomCount--;
-      success--;
+    public static void main(String[] args) {
+        new MineSweeper();
     }
-  }
 
-  private void printMatrix() {
-    System.out.println("===========================");
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < column; j++) {
-        System.out.print(gameMatrix[i][j]);
-      }
-      System.out.print("\n");
+    private void startGame() {
+        int rowIndex = -1;
+        int columnIndex = -1;
+        boolean isFinish = false;
+        while (!isFinish) {
+            boolean isBoundFalse = true;
+            while (isBoundFalse) {
+                System.out.print("Enter row index:");
+                rowIndex = scanner.nextInt();
+                System.out.print("Enter column index:");
+                columnIndex = scanner.nextInt();
+                isBoundFalse =
+                        (rowIndex >= row || rowIndex < 0) || (columnIndex >= column || columnIndex < 0);
+                if (isBoundFalse) {
+                    System.out.println("Again....");
+                }
+            }
+            if (gameMatrix[rowIndex][columnIndex] == '*') {
+                isFinish = true;
+                System.out.println("BOOOMMM!!!");
+                break;
+            }
+            int gain = 0;
+            if (columnIndex < (gameMatrix[0].length - 1)) {
+                gain += gameMatrix[rowIndex][columnIndex + 1] == '*' ? 1 : 0;
+            }
+            if (columnIndex > 0) {
+                gain += gameMatrix[rowIndex][columnIndex - 1] == '*' ? 1 : 0;
+            }
+            if (rowIndex > 0) {
+                gain += gameMatrix[rowIndex - 1][columnIndex] == '*' ? 1 : 0;
+            }
+            if (rowIndex < (gameMatrix.length - 1)) {
+                gain += gameMatrix[rowIndex + 1][columnIndex] == '*' ? 1 : 0;
+            }
+            gameMatrix[rowIndex][columnIndex] = Character.forDigit(gain, 10);
+            success--;
+            if (success == 0) {
+                isFinish = true;
+                System.out.println("CONGRATULATIONS!!!!");
+            }
+            printMatrix();
+        }
     }
-  }
+
+    private void fullMatrix() {
+        gameMatrix = new char[row][column];
+        int len = row * column;
+        int randomCount = len / 4;
+        Map<Integer, Integer> mineIndex = new HashMap<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                gameMatrix[i][j] = '-';
+            }
+        }
+
+        while (randomCount != 0) {
+            Random random = new Random();
+            int randomRow = random.nextInt(0, row);
+            int randomColumn = random.nextInt(0, column);
+            mineIndex.put(randomRow, randomColumn);
+            gameMatrix[randomRow][randomColumn] = '*';
+            randomCount--;
+            success--;
+        }
+    }
+
+    private void printMatrix() {
+        System.out.println("===========================");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(gameMatrix[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
 }
--- java101/src/main/java/com/ayse/minefield/README.md
+++ java101/src/main/java/com/ayse/minefield/README.md
@@ -1,25 +1,28 @@
-# Oyun Kuralları :
+# Game - Minefield
 
-- Oyun metin tabanlıdır.
-- Çift boyutlu diziler üzerinden oynanmalı ve MineSweeper sınıfı içerisinde tasarlanmalı.
-- Matris boyutunu yani satır ve sütun sayısını kullanıcı belirlemeli.
-- Diziye ait eleman sayısının çeyreği (elemanSayisi / 4) kadar rastgele mayın yerleştirilmeli.
-  Örneğin dizi 4x3 boyutunda ise eleman sayısı (satırSayısı * sütunSayısı) formülü ile hesaplanmalı
-  ve boyutu 12 olmalı. Bu durumda mayın sayısı 12 / 4 = 3 adet olmalıdır. (ipucu : bu mayınların
-  konumlarını tutacak ikinci bir dizi oluşturabilirsiniz.)
-- Kullanıcı matris üzerinden bir nokta seçmeli. Nokta seçimi için satır ve sütun değerlerini
-  girmeli.
-- Seçilen noktanın dizinin sınırları içerisinde olup olmadığını kontrol edilmeli ve koşul
-  sağlanmazsa tekrar nokta istenmeli.
-- Kullanıcının girdiği noktada mayın var ise oyunu kaybetmeli.
-- Mayın yok ise, ilgili noktaya değen tüm konumlarına bakılmalı (sağı, solu, yukarısı, aşağısı, sol
-  üst çapraz, sağ üst çapraz, sağ alt çapraz, sol alt çapraz) ve etrafındaki mayınların sayısının
-  toplamı ilgili noktaya yazılmalı. Noktaya değen herhangi bir mayın yok ise "0" değeri atanmalı.
-- Kullanıcı hiç bir mayına basmadan tüm noktaları seçebilirse oyunu kazanmalı.
+## Game Rules
 
-### Kaybetme Senaryosu
+- The game is text-based.
+- It should be played using two-dimensional arrays and designed within the `MineSweeper` class.
+- The user should determine the matrix size, i.e., the number of rows and columns.
+- Random mines should be placed in the array, amounting to a quarter of the total number of elements (elementCount / 4).
+  For example, if the array is 4x3 in size, the number of elements should be calculated using the formula (rowCount *
+  columnCount),
+  resulting in a size of 12. In this case, the number of mines should be 12 / 4 = 3. (Hint: you can create a second
+  array to store the positions of these mines.)
+- The user should select a point on the matrix by entering the row and column values.
+- It should be checked whether the selected point is within the bounds of the array, and if the condition is not met,
+  the point should be requested again.
+- If there is a mine at the point entered by the user, the game should be lost.
+- If there is no mine, all positions adjacent to the relevant point should be checked (right, left, top, bottom,
+  top-left diagonal, top-right diagonal, bottom-right diagonal, bottom-left diagonal), and the total number of mines
+  around should be written to the relevant point. If there are no mines adjacent to the point, the value "0" should be
+  assigned.
+- If the user selects all points without hitting any mines, they should win the game.
 
-Mayınların Konumu
+### Losing Scenario
+
+Mine Locations
 
 *
     -
@@ -29,62 +32,62 @@ Mayınların Konumu
     -
         *
 
-- - - 
+- - -
 ===========================
-Mayın Tarlası Oyuna Hoşgeldiniz !
+Welcome to the minefield game!
 - - - 
 - - - 
 - - - 
-Satır Giriniz : 0
-Sütun Giriniz : 1
+Enter Row : 0
+Enter Column : 1
 ===========================
 
 - 2 -
 
 - - - 
 - - - 
-Satır Giriniz : 2
-Sütun Giriniz : 0
+Enter Row : 2
+Enter Column : 0
 ===========================
 
 - 2 -
 
 - - - 
 0 - -
-Satır Giriniz : 0
-Sütun Giriniz : 2
+Enter Row : 0
+Enter Column : 2
 ===========================
 
 - 2 1
 
 - - - 
 0 - -
-Satır Giriniz : 1
-Sütun Giriniz : 0
+Enter Row : 1
+Enter Column : 0
 ===========================
 
 - 2 1
   1 - -
   0 - -
-  Satır Giriniz : 2
-  Sütun Giriniz : 1
+  Enter Row : 2
+  Enter Column : 1
   ===========================
 - 2 1
   1 - -
   0 1 -
-  Satır Giriniz : 2
-  Sütun Giriniz : 2
+  Enter Row : 2
+  Enter Column : 2
   ===========================
 - 2 1
   1 - -
   0 1 1
-  Satır Giriniz : 1
-  Sütun Giriniz : 2
+  Enter Row : 1
+  Enter Column : 2
   Game Over!!
   ===========================
 
-Kazanma Senaryosu
-Mayınların Konumu
+Winning Scenario
+Mine Locations
 - - - 
 
 -
@@ -95,49 +98,49 @@ Mayınların Konumu
         -
 
 ===========================
-Mayın Tarlası Oyuna Hoşgeldiniz !
+Welcome to the minefield game!
 - - - 
 - - - 
 - - - 
-Satır Giriniz : 0
-Sütun Giriniz : 0
+Enter Row : 0
+Enter Column : 0
 ===========================
 1 - -
 - - - 
 - - - 
-Satır Giriniz : 0
-Sütun Giriniz : 1
+Enter Row : 0
+Enter Column : 1
 ===========================
 1 1 -
 - - - 
 - - - 
-Satır Giriniz : 0
-Sütun Giriniz : 2
+Enter Row : 0
+Enter Column : 2
 ===========================
 1 1 1
 - - - 
 - - - 
-Satır Giriniz : 1
-Sütun Giriniz : 0
+Enter Row : 1
+Enter Column : 0
 ===========================
 1 1 1
 2 - -
 - - - 
-Satır Giriniz : 2
-Sütun Giriniz : 0
+Enter Row : 2
+Enter Column : 0
 ===========================
 1 1 1
 2 - -
 2 - -
-Satır Giriniz : 1
-Sütun Giriniz : 2
+Enter Row : 1
+Enter Column : 2
 ===========================
 1 1 1
 2 - 2
 2 - -
-Satır Giriniz : 2
-Sütun Giriniz : 2
-Oyunu Kazandınız !
+Enter Row : 2
+Enter Column : 2
+You won the game!
 1 1 1
 2 - 2
 2 - 2
