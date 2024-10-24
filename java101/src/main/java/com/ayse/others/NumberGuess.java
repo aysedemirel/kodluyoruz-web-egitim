9cd124e85aa1268378fe957f612d758080692982
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2024-10-24 23:50:52 +0900

    Refactor others package

--- java101/src/main/java/com/ayse/others/NumberGuess.java
+++ java101/src/main/java/com/ayse/others/NumberGuess.java
@@ -5,97 +5,92 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Java dilinde programın 0-100 arasında rastgele seçtiği bir sayıyı kullanıcının tahmin etmesini
- * istediğimiz bir "Sayı Tahmin Oyunu" yapıyoruz.
+ * Write a "Number Guessing Game" where the program randomly selects a number between 0-100 and the user tries to guess it.
+ *
+ * @author aysedemirel
  */
 public class NumberGuess {
 
-  private final int randomNumber;
-  private int right = 5;
-
-  public NumberGuess() {
-    randomNumber = new Random().nextInt(100);
-    guess();
-    // patikaGuess();
-  }
-
-  public static void main(String[] args) {
-    new NumberGuess();
-  }
-
-  private void guess() {
-    int number;
-    Scanner scanner = new Scanner(System.in);
-    while (right > 0) {
-      System.out.print("Guess (between 0-100) : ");
-      number = scanner.nextInt();
-      if (number <= 0 || number >= 101) {
-        right--;
-        System.out.println("Please enter a valid number. Right: " + right);
-      } else if (number != randomNumber) {
-        right--;
-        System.out.println("Wrong!!! Right: " + right);
-        String hint = (number > randomNumber) ? "your number is greater than lucky number"
-            : "your number is lower than lucky number";
-        System.out.println(hint);
-      } else if (number == randomNumber) {
-        System.out.println("WIN!!! Lucky number: " + number);
-        break;
-      }
-      if (right == 0) {
-        System.out.println("You don't have enough right to play..... number was: " + number);
-      }
+    public static void main(String[] args) {
+        int randomNumber = new Random().nextInt(100);
+        guess(randomNumber);
+        // patikaGuess(randomNumber);
     }
-  }
 
-  public void patikaGuess() {
-    Scanner input = new Scanner(System.in);
-    int right = 0;
-    int selected;
-    int[] wrong = new int[5];
-    boolean isWin = false;
-    boolean isWrong = false;
-
-    System.out.println(randomNumber);
-    while (right < 5) {
-      System.out.print("Lütfen tahmininizi giriniz : ");
-      selected = input.nextInt();
-
-      if (selected < 0 || selected > 99) {
-        System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
-        if (isWrong) {
-          right++;
-          System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
-        } else {
-          isWrong = true;
-          System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
+    private static void guess(int randomNumber) {
+        int right = 5;
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        while (right > 0) {
+            System.out.print("Guess (between 0-100) : ");
+            number = scanner.nextInt();
+            if (number <= 0 || number >= 101) {
+                right--;
+                System.out.println("Please enter a valid number. Right: " + right);
+            } else if (number != randomNumber) {
+                right--;
+                System.out.println("Wrong!!! Right: " + right);
+                String hint = (number > randomNumber) ? "your number is greater than lucky number"
+                        : "your number is lower than lucky number";
+                System.out.println(hint);
+            } else if (number == randomNumber) {
+                System.out.println("WIN!!! Lucky number: " + number);
+                break;
+            }
+            if (right == 0) {
+                System.out.println("You don't have enough right to play..... number was: " + number);
+            }
         }
-        continue;
-      }
+    }
 
-      if (selected == randomNumber) {
-        System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + randomNumber);
-        isWin = true;
-        break;
-      } else {
-        System.out.println("Hatalı bir sayı girdiniz !");
-        if (selected > randomNumber) {
-          System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
-        } else {
-          System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
+    private static void guess2(int randomNumber) {
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
+
+        System.out.println(randomNumber);
+        while (right < 5) {
+            System.out.print("Please enter your guess: ");
+            selected = input.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Please enter a value between 0-100.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Too many invalid entries. Remaining rights: " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Your next invalid entry will reduce your rights.");
+                }
+                continue;
+            }
+
+            if (selected == randomNumber) {
+                System.out.println("Congratulations, correct guess! The guessed number: " + randomNumber);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("You entered an incorrect number!");
+                if (selected > randomNumber) {
+                    System.out.println(selected + " is greater than the secret number.");
+                } else {
+                    System.out.println(selected + " is less than the secret number.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Remaining rights: " + (5 - right));
+            }
         }
 
-        wrong[right++] = selected;
-        System.out.println("Kalan hakkı : " + (5 - right));
-      }
-    }
+        if (!isWin) {
+            System.out.println("You lost!");
+            if (!isWrong) {
+                System.out.println("Your guesses: " + Arrays.toString(wrong));
+            }
+        }
 
-    if (!isWin) {
-      System.out.println("Kaybettiniz ! ");
-      if (!isWrong) {
-        System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
-      }
     }
-
-  }
 }
--- java101/src/main/java/com/ayse/others/PalindromicWord.java
+++ java101/src/main/java/com/ayse/others/PalindromicWord.java
@@ -1,42 +1,38 @@
 package com.ayse.others;
 
 /**
- * Java dilinde kullanıcının girdiği kelimenin "Palindromik" olup olmadığı bulan bir program
- * yazıyoruz.
+ * Determine whether the word entered by the user is "Palindromic".
  * <p>
- * Eğer bir kelimenin tersten okunuşu yine aynı kelimeyi veriyorsa o kelime "Palindromik
- * Kelime'dir".
+ * If the reverse reading of a word gives the same word, that word is a "Palindromic Word".
  * <p>
- * Örnek : abba , asa , kavak, kayak
+ * Example: abba, asa, kavak, kayak
+ *
+ * @author aysedemirel
  */
 public class PalindromicWord {
 
-  public PalindromicWord() {
-    System.out.println(isPalindrome("abba"));
-  }
-
-  public static void main(String[] args) {
-    new PalindromicWord();
-  }
-
-  public boolean isPalindrome(String str) {
-    int i = 0, j = str.length() - 1;
-    while (i < j) {
-      if (str.charAt(i) != str.charAt(j)) {
-        return false;
-      }
-      i++;
-      j--;
-    }
-    return true;
-  }
-
-  public boolean isPalindrome2(String str) {
-    String reverse = "";
-    for (int i = str.length() - 1; i >= 0; i--) {
-      reverse += str.charAt(i);
+    public static void main(String[] args) {
+        System.out.println("abba: " + isPalindrome("abba"));
+        System.out.println("java: " + isPalindrome2("java"));
     }
 
-    return str.equals(reverse);
-  }
+    private static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome2(String str) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse.append(str.charAt(i));
+        }
+        return str.contentEquals(reverse);
+    }
 }
